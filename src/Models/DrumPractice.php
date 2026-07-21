6bbffe983fa76834d459c6e97545495164b40a95
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-20 18:00:56 -0700

    Module 6 - DrumPractice model added

---
+++ src/Models/DrumPractice.php
@@ -0,0 +1,23 @@
+// Jennifer Frei - 1405501
+// CS85 Section 1234
+// Module 6A MVC-Based PHP App
+
+<?php
+namespace App\Models;
+// 
+class Routine{
+    // Property Declarations
+    
+    // Define Grooves
+
+    // Define Rudiments
+
+    // Define Fills
+
+
+    // Logic
+
+
+    }
+
+?>
