bbbdb05964fb792baf425afba87745e59abb8b09
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-20 22:37:36 -0700

    Added PracticeController.php with default session duration and view loading

---
+++ src/Controllers/PracticeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\DrumPractice;
+
+class PracticeController
+{
+    public function index(int $minutes = 30):void
+    {
+        // Instantiate model with session duration
+        $practice = new DrumPractice($minutes);
+        // Loads view to display formatted routine
+        reuire__DIR__ . '/../../views/practice.php';
+    }
+}
+?>
