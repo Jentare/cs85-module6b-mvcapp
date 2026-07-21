af98c58441d4e2bc09dff6e21ed99e9fb7f690ba
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-21 02:57:22 -0700

    updated DrumPractice.php with tier settings for 15, 30, 45, 60 minute sessions. Split rudiments and fills depending on practice time. fixed tab spacing

--- src/Models/DrumPractice.php
+++ src/Models/DrumPractice.php
@@ -8,7 +8,6 @@ namespace App\Models;
 class DrumPractice{
     // Property Declarations
     public $totalMinutes;
-    public $tierName;
     public $showRudiments = false;
     public $showFills = false;
 
@@ -45,61 +44,85 @@ class DrumPractice{
         // Master Rudiments
         $allRudiments = [
             [
-                'name' => 'Single Stroke Roll',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 Hands | R L R L | R L R L | R L R L | R L R L \n"
+            'name' => 'Single Stroke Roll',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+            Hands | R L R L | R L R L | R L R L | R L R L \n"
             ],
             [
-                'name' => 'Double Stroke Roll',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 Hands | R R L L | R R L L | R R L L | R R L L \n"
+            'name' => 'Double Stroke Roll',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+            Hands | R R L L | R R L L | R R L L | R R L L \n"
             ],
             [
-                'name' => 'Single Paradiddle',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 Hands | R L R R | L R L L | R L R R | L R L L \n"
+            'name' => 'Single Paradiddle',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+            Hands | R L R R | L R L L | R L R R | L R L L \n"
             ],
             [
-                'name' => 'Flam Accent',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 Hands | lR L R | rL R L | lR L R | rL R L \n"
+            'name' => 'Flam Accent',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+            Hands | lR L R | rL R L | lR L R | rL R L \n"
             ]
         ];
 
         // Master Fills
         $allFills = [
             [
-                'name' => 'Snare Fill',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                S | o o o o | o o o o | o o o o | o o o o\n
-                 K | o - - - | o - - - | o - - - | o - - -"
+            'name' => 'Snare Fill',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+             S | o o o o | o o o o | o o o o | o o o o\n
+             K | o - - - | o - - - | o - - - | o - - -"
             ],
             [
-                'name' => 'Around the kit Fill',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 S | o o o o | - - - - | - - - - | - - - -\n 
-                T1 | - - - - | o o o o | - - - - | - - - -\n 
-                T2 | - - - - | - - - - | o o o o | - - - -\n 
-                FT | - - - - | - - - - | - - - - | o o o o\n
-                 K | o - - - | o - - - | o - - - | o - - -"
+            'name' => 'Around the kit Fill',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+             S | o o o o | - - - - | - - - - | - - - -\n 
+            T1 | - - - - | o o o o | - - - - | - - - -\n 
+            T2 | - - - - | - - - - | o o o o | - - - -\n 
+            FT | - - - - | - - - - | - - - - | o o o o\n
+             K | o - - - | o - - - | o - - - | o - - -"
             ],
             [
-                'name' => '2-beat Snare to tom Fill',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 S | o o - - | o o - - | - - - - | o o o o\n 
-                T1 | - - o - | - - o - | o o o o | - - - -\n 
-                 K | o - - - | o - - - | o - - - | o - - -"
+            'name' => '2-beat Snare to tom Fill',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+             S | o o - - | o o - - | - - - - | o o o o\n 
+            T1 | - - o - | - - o - | o o o o | - - - -\n 
+             K | o - - - | o - - - | o - - - | o - - -"
             ],
             [
-                'name' => '2-beat Snare Fill',
-                'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
-                 S | - - o o | - - o o | o o - - | - - - -\n 
-                T1 | - o - - | - o - - | - - o o | - - - -\n 
-                T2 | - - - - | - - - - | - - - - | o o - -\n 
-                FT | o - - - | o - - - | - - - - | - - o o\n
-                 K | o - - - | o - - - | o - - - | o - - -"
+            'name' => '2-beat Snare Fill',
+            'tab' => "Count | 1 e + a | 2 e + a | 3 e + a | 4 e + a\n 
+             S | - - o o | - - o o | o o - - | - - - -\n 
+            T1 | - o - - | - o - - | - - o o | - - - -\n 
+            T2 | - - - - | - - - - | - - - - | o o - -\n 
+            FT | o - - - | o - - - | - - - - | - - o o\n
+             K | o - - - | o - - - | o - - - | o - - -"
             ]
         ];
+
+    // TIER LOGIC
+    $tiers = [
+    15 => ['rudimentCount' => 2, 'rudimentTime' => 3.5, 'fillCount' => 0, 'fillTime' => 0, 'grooveTime' => 2.0],
+    30 => ['rudimentCount' => 4, 'rudimentTime' => 3.0, 'fillCount' => 2, 'fillTime' => 3.0, 'grooveTime' => 3.0],
+    45 => ['rudimentCount' => 4, 'rudimentTime' => 4.0, 'fillCount' => 4, 'fillTime' => 3.25, 'grooveTime' => 4.0],
+    60 => ['rudimentCount' => 4, 'rudimentTime' => 5.0, 'fillCount' => 4, 'fillTime' => 5.0, 'grooveTime' => 5.0],
+    ];
+
+    // default to 30 minutes
+    $config = $tiers[$this->totalMinutes] ?? $tiers[30];
+
+    // set groove time
+    $this->timePerGroove = $config['grooveTime'];
+
+    // slice rudiments and set time / visibility
+    $this->rudiments = array_slice($allRudiments, 0, $config['rudimentCount']);
+    $this->timePerRudiment = $config['rudimentTime'];
+    $this->showRudiments = count($this->rudiments) > 0;
+
+    // slice fills and set time / visibility
+    $this->fills = array_slice($allFills, 0, $config['fillCount']);
+    $this->timePerFill = $config['fillTime'];
+    $this->showFills = count($this->fills) > 0;
     }
     public function getSummary(){
         return "Drum Practice Routine";
